3921999b9ed7800e3ad6b4edd765b2818ff6111e
Amy Hwang <728amyh@gmail.com>
2022-08-02 23:27:57 -0400

    set currency format

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilites/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -11,11 +12,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   return (
     <Card>
       <Card.Img
-        // variant="top"
+        variant="top"
         src={imgUrl}
-        height="300px"
-        // style={{ objectFit: "cover" }}
+        height="200px"
+        style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 }
